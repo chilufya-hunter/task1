85cc164146700fa51eef4bb1fd41d8f32be0459d
chilufya <chilufyamwape@gmail.com>
2021-04-25 10:37:34 +0300

    main program

---
+++ test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			uint n;
+			Console.Write("Enter n:");
+			n = uint.Parse(Console.ReadLine());
+
+			double product = 1;
+			if (n % 2 == 0)
+			{
+				for (int i = 2; i <= n; i += 2)
+				{
+					product *= i;
+				}
+			}
+			else
+			{
+				for (int i = 1; i <= n; i += 2)
+				{
+					product *= i;
+				}
+			}
+			Console.Write("product =: "+ product);
+			Console.ReadKey();
+
+
+		}
+	}
+}
+
